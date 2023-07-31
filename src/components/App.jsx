4eabd85a3e5afaf4a818a17bd2c5b2109e27dc98
Tarasdeveloper <tomahavki1@gmail.com>
2023-07-31 15:43:22 +0300

    src added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { useEffect, useState } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import Wrapper from './Wrapper/Wrapper';
+import { nanoid } from '@reduxjs/toolkit';
+
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      setContacts(JSON.parse(savedContacts));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = event => {
+    const contact = {
+      id: nanoid(),
+      name: event.name,
+      tel: event.tel,
+    };
+
+    const existingContact = contacts.find(
+      elem => contact.name.toLowerCase() === elem.name.toLowerCase()
+    );
+
+    if (existingContact) {
+      alert(`${contact.name} is already in contacts.`);
+    } else {
+      setContacts(prevContacts => [contact, ...prevContacts]);
+    }
+  };
+
+  const changeFilter = event => {
+    setFilter(event.currentTarget.value);
+  };
+
+  const deleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+  };
+
+  const filterContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
   );
-};
+
+  return (
+    <Wrapper>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList data={filterContacts} onDeleteContact={deleteContact} />
+    </Wrapper>
+  );
+}
