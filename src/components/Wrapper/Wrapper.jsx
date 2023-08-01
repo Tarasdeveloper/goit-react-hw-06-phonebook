c58d3bb5ec11db84a14ef05f9d6b16c4dce9f28e
Tarasdeveloper <tomahavki1@gmail.com>
2023-08-01 13:44:09 +0300

    Update Wrapper.jsx

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -4,14 +4,12 @@ import { Container } from './Wrapper.styled';
 const Wrapper = ({ title, children }) => {
   return (
     <Container>
-      <h2>{title}</h2>
       <div>{children}</div>
     </Container>
   );
 };
 
 Wrapper.propTypes = {
-  title: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 };
 
